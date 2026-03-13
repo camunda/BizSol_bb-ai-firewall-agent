72e179a764f37e1811550c45c389a4d4c2709cd5
Volker Buzek <dev@metalodge.com>
2026-03-13 16:44:59 +0100

    refactor: remove unused import for TimeUnit in SafeguardPromptClassificationIT

--- src/test/java/io/camunda/bizsol/bb/ai_firewall_agent/SafeguardPromptClassificationIT.java
+++ src/test/java/io/camunda/bizsol/bb/ai_firewall_agent/SafeguardPromptClassificationIT.java
@@ -11,7 +11,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
-import java.util.concurrent.TimeUnit;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import org.assertj.core.api.Assertions;
