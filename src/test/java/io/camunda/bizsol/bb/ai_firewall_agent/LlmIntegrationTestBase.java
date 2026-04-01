fd6c329155d73759472fff1e968739321016dc8d
Volker Buzek <dev@metalodge.com>
2026-04-01 16:42:16 +0200

    refactor: remove unnecessary blank line in LlmIntegrationTestBase

--- src/test/java/io/camunda/bizsol/bb/ai_firewall_agent/LlmIntegrationTestBase.java
+++ src/test/java/io/camunda/bizsol/bb/ai_firewall_agent/LlmIntegrationTestBase.java
@@ -21,7 +21,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-
 /**
  * Shared base class for LLM integration tests that use real LLM calls via AWS Bedrock.
  *
