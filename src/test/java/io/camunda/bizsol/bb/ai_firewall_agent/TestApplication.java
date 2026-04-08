c958b0709ca2b7197979cbf4325e09f281dbfbae
Jan Ladleif <jan.ladleif@camunda.com>
2026-04-08 14:47:53 +0200

    chore: Add comments

--- src/test/java/io/camunda/bizsol/bb/ai_firewall_agent/TestApplication.java
+++ src/test/java/io/camunda/bizsol/bb/ai_firewall_agent/TestApplication.java
@@ -2,5 +2,6 @@ package io.camunda.bizsol.bb.ai_firewall_agent;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/** Test application class for the LLM integration tests. This is required to enable Spring Boot. */
 @SpringBootApplication
 class TestApplication {}
