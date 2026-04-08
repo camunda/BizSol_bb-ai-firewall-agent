f90f025c09c0082b14ff5530deaddc6a6d6da932
Jan Ladleif <jan.ladleif@camunda.com>
2026-04-08 14:25:18 +0200

    chore: Try to fix build errors

--- src/test/java/io/camunda/bizsol/bb/ai_firewall_agent/LlmIntegrationTestBase.java
+++ src/test/java/io/camunda/bizsol/bb/ai_firewall_agent/LlmIntegrationTestBase.java
@@ -19,6 +19,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
 /**
@@ -82,6 +83,9 @@ abstract class LlmIntegrationTestBase {
     }
 
     // -- Fixtures ---------------------------------------------------------------
+    @SpringBootApplication
+    static class TestApplication {}
+
     @Autowired CamundaClient camundaClient;
 
     /**
